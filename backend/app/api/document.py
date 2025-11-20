194efeb471992135caa34640f86a7bfd6bf5aa91
adhikaribibek231 <159928438+adhikaribibek231@users.noreply.github.com>
2025-11-20 12:53:38 +0545

    embedding chunks

--- backend/app/api/document.py
+++ backend/app/api/document.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, Depends, HTTPException, File, UploadFile, Query
 from app.schema.document import docingest, ChunkStrategy
 from app.services.text_extractor import extract_text_from_pdf
 from app.services.chunker import chunk_text
+from app.services.embedings import embed_chunks
 router = APIRouter()
 
 @router.post("/ingestion/", response_model = docingest)
@@ -16,6 +17,11 @@ async def ingest_document(
     chunks = chunk_text(text, chunking_strategy)
     if not chunks:
         raise HTTPException(status_code=400, detail="chunking failed, no chunks created")
+    
+    embeddings = await embed_chunks(chunks)
+    if len(embeddings) != len(chunks):
+        raise HTTPException(status_code=500, detail="embedding failed, not same number of embeddings and chunks")
+    
     return (
         docingest(
             document_id=file.filename,
